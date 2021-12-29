b28814df5faabfcae399409fcbf24ead9ca9377e
carlos <cf8838@gmail.com>
2021-12-28 23:25:03 -0500

    Save complete

--- script.py
+++ script.py
@@ -1,10 +1,33 @@
 from flask import Flask, render_template, request, redirect
+import pandas as pd 
+import os 
+
+
 
 app = Flask(__name__)
+
+app.config["UPLOAD PATH"] = ""
+
 
 
 @app.route('/', methods =["GET","POST"])
 def home():
+    
+    if request.method == "POST":
+        
+        if request.files:
+           
+           uploaded_file = request.files["uploaded_json"]
+           
+           converted_file = pd.read_json(uploaded_file).to_excel("converted_json_file.xlsx")
+        #    converted_file.save(os.path.join(app.config["UPLOAD PATH"], converted_file.xlsx))
+           
+           print("The file has been stored:  ", uploaded_file)
+           
+           print("The file has been converted:  ", converted_file)
+           
+        return redirect(request.url) 
+        
     return render_template("home.html")
 
 @app.route('/about/')
